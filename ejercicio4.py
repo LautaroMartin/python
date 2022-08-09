a2a6011394aec09a24e0c3d7ffab3fb9ea145b9b
LautaroMartin <110904339+LautaroMartin@users.noreply.github.com>
2022-08-09 09:35:39 -0300

    up

---
+++ ejercicio4.py
@@ -0,0 +1,13 @@
+print("Ejercio 4");
+print("-------------------");
+print("Ingrese el numero de partidos ganados");
+pg=int( input());
+print("Ingrese el numero de partidos empatados");
+pe=int( input());
+print("Ingrese el numero de partidos perdidos");
+pp=int( input());
+ppg=pg*3;
+ppe=pe*1;
+ppp=pp=0;
+pt=ppg+ppe+ppp;
+print("El puntaje final es de: ", pt)
