9d504ed274ce992ad602457c8d552d3865dc34ca
LautaroMartin <110904339+LautaroMartin@users.noreply.github.com>
2022-09-13 10:39:10 -0300

    Delete tpe1.py

--- tpe1.py
+++
@@ -1,8 +0,0 @@
-import math
-print("Ejercicio complementario 1")
-print("------------------------------")
-
-pi =3.1416
-r = float( input("Ingrese el radio:"))
-v = 4/3*pi*r**3
-print("El volumen de la esfera es de:", v)
