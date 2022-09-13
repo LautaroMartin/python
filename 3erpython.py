89f08781ed8c96f4856d96082cbc9cb01b0ef74d
LautaroMartin <110904339+LautaroMartin@users.noreply.github.com>
2022-09-13 10:51:02 -0300

    Delete 3erpython.py

--- 3erpython.py
+++
@@ -1,9 +0,0 @@
-print("Promedio de notas");
-print("--------------------------");
-print("Ingrese las 3 notas del alumno");
-n1=int(input("n1: "));
-n2=int(input("n2: "));
-n3=int(input("n3: "));
-print("------------------------------");
-p=int((n1+n2+n3)/3);
-print(p);
